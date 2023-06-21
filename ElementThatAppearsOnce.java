31630caf008a45a0144e46f7c6afdf887bd6657d
Ashutosh Sharma <sharma.ashutosh1010@gmail.com>
2023-06-21 17:09:25 +0530

    binarySearch type

---
+++ ElementThatAppearsOnce.java
@@ -0,0 +1,36 @@
+// question link: https://practice.geeksforgeeks.org/problems/element-appearing-once2552/0?company[]=Qualcomm&company[]=Qualcomm&difficulty[]=1&page=1&query=company[]Qualcommdifficulty[]1page1company[]Qualcomm
+
+public class ElementThatAppearsOnce {
+    public static void main(String[] args) {
+        int[] arr = {5,5,8,8,11,11,12,12,14,14,24,27,27,28,28,31,31,45,45};
+        int res = search(arr,arr.length);
+        System.out.println(res);
+    }
+
+    static int search(int[] A,int N){
+        int start = 0;
+        int end = N-1;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(A[mid]!=A[mid-1] && A[mid]!=A[mid+1]){
+                return A[mid];
+            }
+
+            if(mid%2 == 0){
+                if(A[mid] == A[mid+1])
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+            else{
+                if(A[mid] == A[mid-1]){
+                    start = mid+1;
+                }
+                else
+                    end = mid-1;
+            }
+        }
+        return -1;
+    }
+}
