f9b9c12de562054b2be50014d8a384a502699a41
Ashutosh Sharma <sharma.ashutosh1010@gmail.com>
2023-06-23 15:39:47 +0530

    important-medium level for revision

--- LongestSubarraySumK.java
+++ LongestSubarraySumK.java
@@ -6,25 +6,33 @@ public class LongestSubarraySumK {
     }
 
     int lenOfLongSubarr(int A[], int N, int K){
-        int max = 0;
-        int sum = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(0,-1);
+        int sum = 0;
+        int maxlen = 0;
+        for(int i = 0; i< N; i++){
+            sum += A[i];
 
-        for (int i = 0; i < N; i++) {
-            sum = sum + A[i];
+            // if the sum of elements starting from index 0 to index i is equal to 0
             if(sum == K){
-                max = Math.max(max,i+1);
+                maxlen = Math.max(maxlen, i+1);
             }
 
-            if(map.containsKey(sum-K)){
-                max = Math.max(max, i - map.get(sum-K));
+            // if differernce between sum of elements from index 0 to i is s and
+            // a value of sum that is equal to s-k has occured before at index j then
+            // the sum of the elements from index j to i is equal to k, which can be used to calculate the lenght of the subarray
+            int remaining_sum = sum - K;
+            if(map.containsKey(remaining_sum)){
+                maxlen = Math.max(maxlen, i - map.get(remaining_sum));
             }
 
+            // if the current sum is not present in the hashmap add it
+            // we do not add every sum,index pair because there can be a sum which can be repeated at a later index
+            // but we want to maximize the size of the subarray, so it wont give the correct resutls.
             if(!map.containsKey(sum)){
                 map.put(sum,i);
             }
+
         }
-        return max;
+        return maxlen;
     }
 }
