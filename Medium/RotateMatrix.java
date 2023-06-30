9e05b46ea8fcc12e9be4a2985dbb4eeb2d6ae969
Ashutosh Sharma <sharma.ashutosh1010@gmail.com>
2023-06-30 15:24:13 +0530

    medium-matrix-rotation

---
+++ Medium/RotateMatrix.java
@@ -0,0 +1,66 @@
+package Medium;
+
+public class RotateMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+//        rotateAntiClockwise(arr,arr.length);
+        rotateClockwise(arr);
+    }
+
+    static void rotateClockwise(int[][] matrix){
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        printArray(matrix,n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+        printArray(matrix,n);
+    }
+
+    static void rotateAntiClockwise(int[][] matrix, int n){
+//        transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        printArray(matrix,n);
+        System.out.println();
+//        reverse the rows of the matrix
+        for (int i = 0; i < n/2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-i-1][j];
+                matrix[n-i-1][j] = temp;
+            }
+        }
+        printArray(matrix,n);
+        System.out.println();
+    }
+
+    static void printArray(int[][] arr,int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
+    }
+}
